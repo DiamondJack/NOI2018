9ef4ad11c5731bc3999a6fc28d136cf4324c23a4
immortalCO <immortalco@qq.com>
2018-06-18 13:51:02 +0800

    edit table

--- day2/intelligence/tables/data.py
+++ day2/intelligence/tables/data.py
@@ -8,7 +8,7 @@ ret = [
 	[0,			tl.hn(2),		tl.hn(3),		"无"			],
 	[0,			tl.hn(10),		tl.hn(30),		"无"			],
 	[0,			tl.hn(200),		tl.hn(300),		"S"			],
-	[0,			tl.hn(1000),	tl.hn(10000),	"S"			],
+	[0,			tl.hn(1000),	tl.hn(2000),	"S"			],
 	[0,			HlfN,			HlfM,			"SA"			],
 	[0,			MaxN,			MaxM,			"SA"			],
 	[0,			HlfN,			HlfM,			"SB"			],
