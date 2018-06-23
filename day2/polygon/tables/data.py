8ca09f67c4aba72d045c91b79d4ea1d9c5f982b7
Ruyi Ji <jiruyi910387714@126.com>
2018-06-23 09:42:59 +0800

    add table

--- day2/polygon/tables/data.py
+++ day2/polygon/tables/data.py
@@ -1,33 +1,21 @@
-MaxN = tl.hn(50000)
-MaxM = tl.hn(100000)
-HlfN = tl.hn(10000)
-HlfM = tl.hn(30000)
-
-ret = [
-	["测试点",	"$n\\le $",		"$m\\le$",		"特殊性质"	],
-	[0,			tl.hn(2),		tl.hn(3),		"无"			],
-	[0,			tl.hn(10),		tl.hn(30),		"无"			],
-	[0,			tl.hn(200),		tl.hn(300),		"S"			],
-	[0,			tl.hn(1000),	tl.hn(2000),	"SA"			],
-	[0,			HlfN,			HlfM,			"SA"			],
-	[0,			MaxN,			MaxM,			"SA"			],
-	[0,			HlfN,			HlfM,			"SB"			],
-	[0,			MaxN,			MaxM,			"SB"			],
-	[0,			MaxN,			MaxM,			"SB"			],
-	[0,			HlfN,			"$n$",			"SC"		],
-	[0,			MaxN,			"$n$",			"C"			],
-	[0,			MaxN,			"$n$",			"C"			],
-	[0,			HlfN,			HlfM,			"SD"		],
-	[0,			HlfN,			HlfM,			"D"			],
-	[0,			MaxN,			MaxM,			"D"			],
-	[0,			MaxN,			MaxM,			"D"			],
-	[0,			HlfN,			HlfM,			"S"			],
-	[0,			HlfN,			HlfM,			"S"			],
-	[0,			MaxN,			MaxM,			"无"			],
-	[0,			MaxN,			MaxM,			"无"			],
-]
-
-for i in range(1, len(ret)):
-	ret[i][0] = str(i)
+ret = [["$n$","$K$","其他约定"]]
+for i in range(4):
+	ret.append(["$\leq 20$", "$\leq 3$", "无"])
+for i in range(6):
+	ret.append(["$\leq 1000$", "$=1$", "无"])
+for i in range(6):
+	ans = ["$\leq 1000$", "$\leq 2$"]
+	if i < 3:
+		ans.append("无")
+	else:
+		ans.append("保证每一个点最多只有两个孩子")
+	ret.append(ans)
+for i in range(4):
+	ans = ["$\leq 1000$", "$\leq 3$"]
+	if i < 2:
+		ans.append("无")
+	else:
+		ans.append("保证每一个点最多只有两个孩子")
+	ret.append(ans)
 
 return merge_ver(ret)
