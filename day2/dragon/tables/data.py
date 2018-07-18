3c0930a3c21740fb3a48904bd46e2ca377b87f4b
ufozgg <ufozgg@qq.com>
2018-07-19 04:43:04 +0800

    data d2p1

--- day2/dragon/tables/data.py
+++ day2/dragon/tables/data.py
@@ -9,7 +9,7 @@ for i in range(20):
 		ATK="$= 1$"
 		etc="无"
 		#Ans = Max(a)
-	elif(i<5):
+	elif(i<4):
 		n="$\le 10^5$";
 		m="$= 1$"
 		p="$= 1$"
@@ -17,23 +17,31 @@ for i in range(20):
 		ATK="$\le 10^5$"
 		etc="无"
 		#Ans = Max(a/ATK)
-	elif(i<8):
+	elif(i<7):
+		n="$\le 10^3$";
+		m="$\le 10^3$"
+		p="$\le 10^5$"
+		a="$\le 10^5$"
+		ATK="$\le 10^5$"
+		etc="特性1、特性2"
+		#for
+	elif(i<10):
 		n="$= 1$"
 		m="$= 1$"
 		p="$\le 10^8$"
 		a="$\le 10^8"
 		ATK="$\le 10^6$"
-		etc="对于任意的$i$，$a_i \le p_i$"
+		etc="特性1"
 		#exgcd
-	elif(i<11):
+	elif(i<13):
 		n="$= 1$"
 		m="$= 1$"
 		p="$\le 10^8$"
 		a="$\le 10^8"
 		ATK="$\le 10^6$"
-		etc="对于任意的$i$，$a_i \le p_i$"
+		etc="特性1"
 		#exgcd quick add
-	elif(i<14):
+	elif(i<15):
 		n="$= 10^5$"
 		m="$= 10^5$"
 		p="$= 1$"
@@ -47,14 +55,14 @@ for i in range(20):
 		p="所有 $p_i$ 是质数"
 		a="$\le 10^{12}$"
 		ATK="$\le 10^6$"
-		etc="对于任意的$i$，$a_i \le p_i$"
+		etc="特性1"
 	else:
 		n=" $\le$ 10^5"
 		m=" $\le$ 10^5"
 		p="无特殊限制"
 		a="$\le 10^{12}$"
 		ATK="$\le 10^6$"
-		etc="对于任意的$i$，$a_i \le p_i$"
+		etc="特性1"
 	row = [i+1,n,m,p,a,ATK,etc]
 	ret.append(row)
 return merge_ver(ret)
